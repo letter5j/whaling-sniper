badb554b5295d34d6e2b38673bcdd82158e5b279
letter5j <seanletter5j@Gmail.com>
2021-11-14 19:20:14 +0800

    chore: update deta.json
    update deta.json and remove useless environment variable

--- settings.py
+++ settings.py
@@ -5,6 +5,5 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     TWITTER_BEARER_TOKEN: str
-    OUTPUT_BASE_PATH: str
     STORAGE_SERVICE_ENDPOINT: Optional[str]
     DELTA_MINUTES_BEFORE_NOW: Optional[int] = 5
